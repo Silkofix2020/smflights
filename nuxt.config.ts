c7c39723c4ddc217e93c8ebb9088cb13bd9735d7
Silkofix2020 <vkfeik2014@gmail.com>
2024-06-17 16:47:29 +0300

    Add prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      routes: ["/pricacy"],
+    },
+  },
   modules: ["@pinia/nuxt"],
   css: ["~/assets/scss/main.scss"],
 });
