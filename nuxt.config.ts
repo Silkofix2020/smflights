9d17291d9d5b7d346ce2a9702cd4f85fc1a69408
Silkofix2020 <vkfeik2014@gmail.com>
2024-06-18 13:57:59 +0300

    Add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "@nuxtjs/sitemap"],
+  sitemap: {
+    hostname: "https://yourwebsite.com",
+    gzip: true,
+    routes: [
+      "/about",
+      "/result",
+      "/privacy",
+      // дополнительные маршруты
+    ],
+  },
   plugins: ["~/plugins/v-click-outside.js", "~/plugins/loading.js"],
   nitro: {
     prerender: {
       routes: ["/"],
     },
   },
-  modules: ["@pinia/nuxt"],
   css: ["~/assets/scss/main.scss"],
 });
