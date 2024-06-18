ee16908d1b64a6429b8e566569733e9270edd614
Silkofix2020 <vkfeik2014@gmail.com>
2024-06-18 04:46:27 +0300

    Add fetch to form data

--- server/api/sendToGoogleSheets.js
+++ server/api/sendToGoogleSheets.js
@@ -1,36 +1,41 @@
-export default function handler(req, res) {
+import { eventHandler, send } from "h3";
+
+export default eventHandler(async (req, res) => {
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.status(200).end();
+    res.statusCode = 200;
+    res.end();
     return;
   }
 
   if (req.method === "POST") {
-    const data = req.body;
+    const data = await readBody(req);
 
-    // Отправка данных в Google Apps Script
-    fetch(
-      "https://script.google.com/macros/s/AKfycbyUJ4SBIsL0OJaKNij97aNdI97sbth5EC7lCONOIKi0wiqUOW23LSBUxlb51gHSOWWJ0Q/exec",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        res.status(200).json(result);
-      })
-      .catch((error) => {
-        console.error("Error sending data:", error);
-        res.status(500).json({ error: "Error sending data" });
-      });
+    try {
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbyUJ4SBIsL0OJaKNij97aNdI97sbth5EC7lCONOIKi0wiqUOW23LSBUxlb51gHSOWWJ0Q/exec",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      const result = await response.json();
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      send(res, 200, result);
+    } catch (error) {
+      console.error("Error sending data:", error);
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      send(res, 500, { error: "Error sending data" });
+    }
   } else {
-    res.setHeader("Allow", ["POST", "OPTIONS"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    res.setHeader("Allow", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    send(res, 405, `Method ${req.method} Not Allowed`);
   }
-}
+});
