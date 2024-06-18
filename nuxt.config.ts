8eb59679a631c8db192b307f4c6cb58d476e13c6
Silkofix2020 <vkfeik2014@gmail.com>
2024-06-18 16:25:36 +0300

    Add app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,37 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "SM Flights",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Описание вашего сайта",
+        },
+        { name: "keywords", content: "aviation, flights, booking" },
+        // Open Graph meta tags
+        {
+          property: "og:title",
+          content: "SM Flights - Your Reliable Flight Booking Service",
+        },
+        {
+          property: "og:description",
+          content: "Book your flights easily and quickly with SM Flights.",
+        },
+        {
+          property: "og:image",
+          content: "https://smflights.vercel.app/og-image.png",
+        },
+        { property: "og:url", content: "https://smflights.vercel.app" },
+        { property: "og:type", content: "website" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxtjs/sitemap"],
   sitemap: {
